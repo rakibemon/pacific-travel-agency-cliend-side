52e426e14b19f628a01907297b1fff14f093c851
rakibemon <rakibemon03@gmail.com>
2021-10-31 00:00:42 +0600

    Button problem fixed

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -3,9 +3,9 @@ import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 const SignUp = () => {
     return (
-        <div style={{ marginTop: '120px' }}>
+        <div style={{ marginTop: '120px' }} className='text-center'>
             <h4> Please Login for now. Later this Sign up page will be modified</h4>
-            <Link to='/login'> <Button className="button"></Button></Link>
+            <Link to='/login'> <Button className="button">Go to Login</Button></Link>
         </div>
     );
 };
